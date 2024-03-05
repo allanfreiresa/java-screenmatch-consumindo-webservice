b9d4a90d23c28887cc9ba9a54925eb92ec0161e9
Allan Freire(ACBZ) <Allan_Freire@acbz.com.br>
2024-03-05 16:36:31 -0300

    Criando desafio CEP

---
+++ src/br/com/alura/desafioCep/Buscador/Buscador.java
@@ -0,0 +1,7 @@
+package br.com.alura.desafioCep.Buscador;
+
+public class Buscador {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/br/com/alura/desafioCep/Buscador/Endereco.java
@@ -0,0 +1,4 @@
+package br.com.alura.desafioCep.Buscador;
+public record Endereco(String cep, String logradouro,String complemento,
+                       String localidade, String uf ) {
+}
